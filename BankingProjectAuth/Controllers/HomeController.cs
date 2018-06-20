7a7334debdc0662bc56fe8f467cff76310c4741a
Petar Hristov <nightelada@gmail.com>
2018-06-21 00:20:46 +0300

    added more shitty css

--- BankingProjectAuth/Controllers/HomeController.cs
+++ BankingProjectAuth/Controllers/HomeController.cs
@@ -6,15 +6,42 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BankingProjectAuth.Models;
 using Microsoft.AspNetCore.Authorization;
+using BankingProjectAuth.Data;
+using Microsoft.AspNetCore.Identity;
+using BankingProjectAuth.Models.BankingAccountViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace BankingProjectAuth.Controllers
 {
     public class HomeController : Controller
     {
-        [Authorize]
-        public IActionResult Index()
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            TestViewModel tvm = new TestViewModel();
+            ApplicationUser currentUser = await _userManager.GetUserAsync(HttpContext.User);
+
+            IEnumerable<BankingAccount> bankingAccounts = _context.BankingAccount.Include(b => b.Currency)
+                .Include(b => b.User)
+                .Include(b => b.Cards);
+
+            IEnumerable<Card> cards = _context.Card.ToList();
+
+            tvm.Accounts = bankingAccounts;
+            tvm.Cards = cards;
+
+            return View(tvm);
+
         }
 
         public IActionResult About()
